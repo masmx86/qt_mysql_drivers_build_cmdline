b164b94237ed2ff521a290974e5e9caafc035f8f
masmx86 <163689544+masmx86@users.noreply.github.com>
2024-09-30 21:58:30 +0930

    fix msvc wrong path

--- build-mysqldrivers.bat
+++ build-mysqldrivers.bat
@@ -1,8 +1,6 @@
-@echo off
-
-REM https://github.com/masmx86
+REM Author:        https://github.com/masmx86
 REM Created:       2024-06-01
-REM Last Modified: 2024-06-06
+REM Last Modified: 2024-06-04
 
 REM Tested:        Windows 11 + MSVS2022 + Qt6.7.1
 
@@ -11,10 +9,11 @@ REM mysql header files path:
 REM   -DMySQL_INCLUDE_DIR="D:\mysql_qt\include"
 REM mysql lib file path:
 REM   -DMySQL_LIBRARY="D:\mysql_qt\lib\libmysql.lib"
-
+REM
 REM CAUTION: DO NOT SPELL "MySQL_..." AS "MYSQL_..."
 REM          Otherwise <PATH> will not be recognised
 
+@echo off
 if "%1"=="" goto USAGE
 
 :BUILD
@@ -22,7 +21,7 @@ set PATH=%PATH%;D:\Qt\Tools\CMake_64\bin;D:\Qt\Tools\Ninja
 D:
 cd D:\Qt\%1\Src\qtbase\src\plugins\sqldrivers
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-call D:\Qt\%1\msvc2019_64\bin\qt-cmake.bat -G "Ninja Multi-Config" . -DMySQL_INCLUDE_DIR="D:\mysql_qt\include" -DMySQL_LIBRARY="D:\mysql_qt\lib\libmysql.lib" -DCMAKE_INSTALL_PREFIX="D:\Qt\%1\msvc2019_64" -DCMAKE_CONFIGURATION_TYPES=Release;Debug
+call D:\Qt\%1\msvc2022_64\bin\qt-cmake.bat -G "Ninja Multi-Config" . -DMySQL_INCLUDE_DIR="D:\mysql_qt\include" -DMySQL_LIBRARY="D:\mysql_qt\lib\libmysql.lib" -DCMAKE_INSTALL_PREFIX="D:\Qt\%1\msvc2022_64" -DCMAKE_CONFIGURATION_TYPES=Release;Debug
 ninja
 ninja install
 goto EXIT
